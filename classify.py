e6be6785d97fbd190633b6f81eca56f8708436f8
kazuma313 <koerzoelmatondang@gmail.com>
2023-12-01 21:43:47 +0700

    build arsitektur

---
+++ classify.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from tensorflow.keras.models import Model as KerasModel
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+
+
+class Classifier:
+    def __init__():
+        self.model = 0
+
+    def predict(self, x):
+        if x.size == 0:
+            return []
+        return self.model.predict(x)
+
+    def fit(self, x, y):
+        return self.model.train_on_batch(x, y)
+
+    def get_accuracy(self, x, y):
+        return self.model.test_on_batch(x, y)
+
+    def load(self, path):
+        self.model.load_weights(path)
+
+    def summary(self):
+      return self.model.summary()
+
+class Meso4(Classifier):
+    image_dimension = (256, 256, 3)
+    def __init__(self, learning_rate=0.001):
+      self.model = self.init_model()
+      optimizer = Adam(learning_rate = learning_rate)
+      self.model.compile(optimizer = optimizer, loss = 'mean_squared_error', metrics = ['accuracy'])
+
+    def init_model(self):
+        x = Input(shape = self.image_dimension)
+
+        x1 = Conv2D(8, (3, 3), padding='same', activation = 'relu')(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
+
+        x2 = Conv2D(8, (5, 5), padding='same', activation = 'relu')(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
+
+        x3 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
+
+        x4 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation = 'sigmoid')(y)
+
+        return Model(inputs = x, outputs = y)
+    
+    
+
+class MesoInception4(Classifier):
+    image_dimension = (256, 256, 3)
+    def __init__(self, learning_rate = 0.001):
+        self.model = self.init_model()
+        optimizer = Adam(lr = learning_rate)
+        self.model.compile(optimizer = optimizer, loss = 'mean_squared_error', metrics = ['accuracy'])
+    
+    def InceptionLayer(self, a, b, c, d):
+        def func(x):
+            x1 = Conv2D(a, (1, 1), padding='same', activation='relu')(x)
+            
+            x2 = Conv2D(b, (1, 1), padding='same', activation='relu')(x)
+            x2 = Conv2D(b, (3, 3), padding='same', activation='relu')(x2)
+            
+            x3 = Conv2D(c, (1, 1), padding='same', activation='relu')(x)
+            x3 = Conv2D(c, (3, 3), dilation_rate = 2, strides = 1, padding='same', activation='relu')(x3)
+            
+            x4 = Conv2D(d, (1, 1), padding='same', activation='relu')(x)
+            x4 = Conv2D(d, (3, 3), dilation_rate = 3, strides = 1, padding='same', activation='relu')(x4)
+
+            y = Concatenate(axis = -1)([x1, x2, x3, x4])
+            
+            return y
+        return func
+    
+    def init_model(self):
+        x = Input(shape = self.image_dimension)
+        
+        x1 = self.InceptionLayer(1, 4, 4, 2)(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
+        
+        x2 = self.InceptionLayer(2, 4, 4, 2)(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)        
+        
+        x3 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
+        
+        x4 = Conv2D(16, (5, 5), padding='same', activation = 'relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+        
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation = 'sigmoid')(y)
+
+        return KerasModel(inputs = x, outputs = y)
+    
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224):
+    """
+    Reads an image from filename, turns it into a tensor
+    and reshapes it to (img_shape, img_shape, colour_channel).
+    """
+    # Read in target file (an image)
+    img = tf.io.read_file(filename)
+
+    # Decode the read file into a tensor & ensure 3 colour channels
+    # (our model is trained on images with 3 colour channels and sometimes images have 4 colour channels)
+    img = tf.image.decode_image(img, channels=3)
+
+    # Resize the image (to the same size our model was trained on)
+    img = tf.image.resize(img, size = [img_shape, img_shape])
+
+    # Rescale the image (get all values between 0 and 1)
+    img = img/255.
+    return img
+
+
+result = ""
+model = Meso4()
+model.load(".\model\Meso4_DF.h5")
+img = load_and_prep_image(filename=r".\data\hard_1_1111.jpg", img_shape=256)
+predict = model.predict(tf.expand_dims(img, axis=0).numpy())
+
+if predict > 0.5:
+    result = "real"
+else:
+    result = "fake"
+    
+print(f"=====Hasil klasifikasi====")
+print(np.squeeze(predict))
+print("hasil : ", result)
+
+
